cb1ce43c4c73e5396122824c3092a9c48c94f4a6
prdpta <pradiptaputramei01@gmail.com>
2025-11-02 15:18:53 +0700

    membuat kolom untuk input kota

--- app/src/main/java/com/example/questuserinput_037/Praktikum4.kt
+++ app/src/main/java/com/example/questuserinput_037/Praktikum4.kt
@@ -94,3 +94,14 @@ fun PraktikumEmpat(modifier: Modifier = Modifier) {
 
             Spacer(Modifier.height(8.dp))
 
+            // Input Kota
+            OutlinedTextField(
+                value = kota,
+                onValueChange = { kota = it },
+                label = { Text("Kota Asal") },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(Modifier.height(8.dp))
+
